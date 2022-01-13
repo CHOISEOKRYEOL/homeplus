e77183bd427c1f0bcec4dc8e8178c6c98cb67e2a
CHOISEOKRYEOL <zlshsktl@naver.com>
2022-01-13 15:55:51 +0900

    .

---
+++ main.js
@@ -0,0 +1,15 @@
+
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 300,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
