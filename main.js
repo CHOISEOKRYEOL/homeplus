32eef97386d41523e9ab7b4851e93e1309972ca7
CHOISEOKRYEOL <zlshsktl@naver.com>
2022-01-14 16:36:00 +0900

    .

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ var swiper = new Swiper(".swiper", {
     },
 });
 
+let img = document.querySelector(".item > a");
+let itemNav = document.querySelector(".item_nav");
+// img.addEventListener("mouseover", () => {
+//     itemNav.classList.add("show");
+// });
+// img.addEventListener("mouseout", () => {
+//     itemNav.classList.remove("show");
+// });
+document.querySelectorAll(".content1_items ul li div").forEach(el => {
+    img.addEventListener("mouseover", () => {
+        el.classList.add("show");
+    });
+    img.addEventListener("mouseout", () => {
+        el.classList.remove("show");
+    })
+});
+
