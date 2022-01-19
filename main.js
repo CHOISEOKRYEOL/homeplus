c292be546b94bd2d2df0bbc9cec15141e61c9974
CHOISEOKRYEOL <zlshsktl@naver.com>
2022-01-19 22:36:11 +0900

    .

--- main.js
+++ main.js
@@ -11,81 +11,148 @@ var swiper = new Swiper(".swiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    autoplay: {
+        delay: 5000,
+    },
 });
 
 // img hover시 생성
-// let item1 = document.querySelector(".content1_menu .menu_btn > ul > li > ul > li > a > img");
-// console.log(item1);
-// item1.addEventListener("mouseover", () => {
-//     document.querySelectorAll(".content1_menu .menu_btn .item.one > div").forEach(el => {
-//         el.classList.add("show");
-//     });
-// });
-// item1.addEventListener("mouseout", () => {
-//     document.querySelectorAll(".content1_menu .menu_btn .item.one > div").forEach(el => {
-//         el.classList.remove("show");
-//     });
-// });
-// let item2 = document.querySelector(".content1_items .items.one .item.two a img");
-// item2.addEventListener("mouseover", () => {
-//     document.querySelectorAll(".content1_items .items.one .item.two div").forEach(el => {
-//         el.classList.add("show");
-//     });
-// });
-// item2.addEventListener("mouseout", () => {
-//     document.querySelectorAll(".content1_items > .items.one > .item.two > div").forEach(el => {
-//         el.classList.remove("show");
-//     });
-// });
-// let item3 = document.querySelector(".content1_items > .items.one > .item.three > a > img");
-// item3.addEventListener("mouseover", () => {
-//     document.querySelectorAll(".content1_items > .items.one > .item.three > div").forEach(el => {
-//         el.classList.add("show");
-//     });
-// });
-// item3.addEventListener("mouseout", () => {
-//     document.querySelectorAll(".content1_items > .items.one > .item.three > div").forEach(el => {
-//         el.classList.remove("show");
-//     });
-// });
-// let item4 = document.querySelector(".content1_items > .items.one > .item.four > a > img");
-// item4.addEventListener("mouseover", () => {
-//     document.querySelectorAll(".content1_items > .items.one > .item.four > div").forEach(el => {
-//         el.classList.add("show");
-//     });
-// });
-// item4.addEventListener("mouseout", () => {
-//     document.querySelectorAll(".content1_items > .items.one > .item.four > div").forEach(el => {
-//         el.classList.remove("show");
-//     });
-// });
-// let item5 = document.querySelector(".content1_items > .items.one > .item.five > a > img");
-// item5.addEventListener("mouseover", () => {
-//     document.querySelectorAll(".content1_items > .items.one > .item.five > div").forEach(el => {
-//         el.classList.add("show");
-//     });
-// });
-// item5.addEventListener("mouseout", () => {
-//     document.querySelectorAll(".content1_items > .items.one > .item.five > div").forEach(el => {
-//         el.classList.remove("show");
-//     });
-// });
-// let item6 = document.querySelector(".content1_items > .items.one > .item.six > a > img");
-// item6.addEventListener("mouseover", () => {
-//     document.querySelectorAll(".content1_items > .items.one > .item.six > div").forEach(el => {
-//         el.classList.add("show");
-//     });
-// });
-// item6.addEventListener("mouseout", () => {
-//     document.querySelectorAll(".content1_items > .items.one > .item.six > div").forEach(el => {
-//         el.classList.remove("show");
-//     });
-// });
+let item1 = document.querySelector(".content1_menu .items > ul > .item.one > a > img");
+item1.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content1_menu .items .item.one > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item1.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content1_menu .items .item.one > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+let item2 = document.querySelector(".content1_menu .items > ul > .item.two > a > img");
+item2.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content1_menu .items .item.two > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item2.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content1_menu .items .item.two > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+let item3 = document.querySelector(".content1_menu .items > ul > .item.three > a > img");
+item3.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content1_menu .items .item.three > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item3.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content1_menu .items .item.three > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+let item4 = document.querySelector(".content1_menu .items > ul > .item.four > a > img");
+item4.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content1_menu .items .item.four > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item4.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content1_menu .items .item.four > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+let item5 = document.querySelector(".content1_menu .items > ul > .item.five > a > img");
+item5.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content1_menu .items .item.five > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item5.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content1_menu .items .item.five > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+let item6 = document.querySelector(".content1_menu .items > ul > .item.six > a > img");
+item6.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content1_menu .items .item.six > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item6.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content1_menu .items .item.six > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+
+let item7 = document.querySelector(".content2_menu .items > ul > .item.one > a > img");
+item7.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content2_menu .items .item.one > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item7.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content2_menu .items .item.one > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+let item8 = document.querySelector(".content2_menu .items > ul > .item.two > a > img");
+item8.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content2_menu .items .item.two > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item8.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content2_menu .items .item.two > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+let item9 = document.querySelector(".content2_menu .items > ul > .item.three > a > img");
+item9.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content2_menu .items .item.three > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item9.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content2_menu .items .item.three > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+let item10= document.querySelector(".content2_menu .items > ul > .item.four > a > img");
+item10.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content2_menu .items .item.four > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item10.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content2_menu .items .item.four > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+let item11 = document.querySelector(".content2_menu .items > ul > .item.five > a > img");
+item11.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content2_menu .items .item.five > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item11.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content2_menu .items .item.five > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+let item12 = document.querySelector(".content2_menu .items > ul > .item.six > a > img");
+item12.addEventListener("mouseover", () => {
+    document.querySelectorAll(".content2_menu .items .item.six > div").forEach(el => {
+        el.classList.add("show");
+    });
+});
+item12.addEventListener("mouseout", () => {
+    document.querySelectorAll(".content2_menu .items .item.six > div").forEach(el => {
+        el.classList.remove("show");
+    });
+});
+
 
 // menu 클릭시 ul 생성, 삭제
-// let $menu_btn = $(".menu_btn");
-// console.log($menu_btn);
-// $menu_btn.find("ul > ul").hide();
 
 $(document).ready(function(){
 
